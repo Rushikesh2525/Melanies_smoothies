bcd757590d620f92210bb6a7c48e16b076b3c1cf
Rushikesh2525 <khandagalerushikesh2525@gmail.com>
2025-01-18 11:14:49 +0530

    Update streamlit_app.py
    changes for fruits we chosen should shown in API chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -26,10 +27,11 @@ ingredients_list =st.multiselect(
 if ingredients_list:
     # st.write(ingredients_list)
     # st.text(ingredients_list)
-    ingredients_string = ''
-    
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+            ingredients_string = ''
+            for fruit_chosen in ingredients_list:
+                        ingredients_string += fruit_chosen + ' '
+                        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+                        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
         
     # st.write(ingredients_string)
 
@@ -46,11 +48,9 @@ if ingredients_list:
         
        st.success('Your Smoothie is ordered',icon="✅")
                 
-# New section to import smoothiefroot nutrition imformation
 
-import requests
+
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
 sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
         
     
